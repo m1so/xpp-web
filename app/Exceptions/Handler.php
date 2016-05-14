e1f1c1bbddd3ff3e110ea02121b552970a558c77
Michal Baumgartner <miso.baumgartner@gmail.com>
2016-05-14 20:34:05 +0200

    Handle TokenMismatchException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,15 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        if ($e instanceof \Illuminate\Session\TokenMismatchException) {
+            return redirect()
+                ->back()
+                ->withInput($request->except('password'))
+                ->with([
+                    'message' => 'Validation token expired, please try again.'
+            ]);
+        }
+
         return parent::render($request, $e);
     }
 }
