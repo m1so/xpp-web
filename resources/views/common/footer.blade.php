c8988f88c6659dbd1e25f59d598c7a66b23b2444
Michal Baumgartner <miso.baumgartner@gmail.com>
2016-05-29 21:41:10 +0200

    Footer update

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,7 +1,9 @@
 <footer class="main-footer">
     <div class="container-fluid">
         <div class="pull-right hidden-xs">
-            <a href="/help/changelog">{{ config('xppweb.version', '') }}</a>
+            <a href="https://github.com/m1so/xpp-web"><i class="fa fa-github"></i> Source </a>
+            |
+            <a href="/help/changelog">v{{ config('xppweb.version', '') }}</a>
         </div>
         <strong>&copy; Michal Baumgartner</strong>
     </div>
